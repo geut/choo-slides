4efd7c3de518cdbdf609692f30b828978c2b16ef
Diego Paez <paez.di@gmail.com>
2018-02-13 17:19:39 -0300

    Update: receive not found view from state

--- views/slide.js
+++ views/slide.js
@@ -1,11 +1,9 @@
-const NotFoundView = require('./404')
-
 module.exports = slideView
 
 function slideView (state, emit) {
   state.params.slideIdx = state.params.slideIdx || 0
   const idx = Number(state.params.slideIdx)
-  if (Number.isNaN(idx) || idx >= state.chooSlides.slides.length) return NotFoundView(state, emit)
+  if (Number.isNaN(idx) || idx >= state.chooSlides.slides.length) return state.chooSlides.notFoundView(state, emit)
   state.chooSlides.current = Number(state.params.slideIdx)
   return state.chooSlides.slides[state.chooSlides.current](state, emit)
 };
