c70e41a7ca37719ffcbdaa6da07fb16fe597848b
Diego Paez <paez.di@gmail.com>
2018-02-13 17:17:51 -0300

    Update: notas -> notes

--- components/note.js
+++ components/note.js
@@ -26,7 +26,7 @@ const Note = (children, emit) => {
 
   return html`
         <section class="${menuStyle} w-50 w-40-m w-auto-ns overflow-y-auto absolute items-center bottom-1 left-2 justify-center">
-            <button class="outline-transparent pointer f5 no-underline black bg-animate bg-transparent hover-white inline-flex items-center pa3 ba b--transparent border-box mr4" onclick="${toogleNotes}"> Notas ${children.on ? '▼' : '▲'} </button>
+            <button class="outline-transparent pointer f5 no-underline black bg-animate bg-transparent hover-white inline-flex items-center pa3 ba b--transparent border-box mr4" onclick="${toogleNotes}"> Notes ${children.on ? '▼' : '▲'} </button>
             <aside class="overflow-x-hidden menu ${children.on ? 'menu--open' : ''} db-l w-100 tc mv0 f3 fw3 f5-l bg-black-80 white-90">
                 <div class="pb2 ph4-l ph3 ph5-xl tl measure-wide">
                     ${children.notes ? children.notes() : ''}
